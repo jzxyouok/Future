f124519267d704e05aa12e78bde4487deb404de3
zhiy <zhengzycn@gmail.com>
2019-07-02 00:30:40 +0800

    Initial Commit

---
+++ Future/Future.h
@@ -0,0 +1,19 @@
+//
+//  Future.h
+//  Future
+//
+//  Created by zhiy on 2019/7/2.
+//  Copyright © 2019 zhiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Future.
+FOUNDATION_EXPORT double FutureVersionNumber;
+
+//! Project version string for Future.
+FOUNDATION_EXPORT const unsigned char FutureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Future/PublicHeader.h>
+
+
